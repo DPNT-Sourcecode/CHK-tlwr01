8ba5c8d5681fe9ed1cd3dde8171d959545db7c3b
sbx_user1051 <sbx_user1051@169.254.13.173>
2025-03-04 00:05:42 +0000

    Tue Mar 04 00:05:42 UTC 2025

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -3,11 +3,12 @@ from solutions.SUM import sum_solution
 
 class TestSum():
     def test_sum(self):
-        assert sum_solution.computer(1, 2) == 3
-        assert sum_solution.computer(0,0) == 0
-        assert sum_solution.computer(10,20) == 0
-        assert sum_solution.computer(50,50) == 0
-        assert sum_solution.computer(99,1) == 0
+        assert sum_solution.compute(1, 2) == 3
+        assert sum_solution.compute(0,0) == 0
+        assert sum_solution.compute(10,20) == 30
+        assert sum_solution.compute(50,50) == 100
+        assert sum_solution.compute(99,1) == 100
         print("All tests passed for SUM challenege!")
+
 
 
